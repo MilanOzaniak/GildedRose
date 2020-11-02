e162b29f7d3ac1bc52f33731aafb44758504be0e
Ozaniak <Ozaniak@PC>
2020-11-02 01:12:36 +0100

    hotovo

--- GildedRose/Inventory.cs
+++ GildedRose/Inventory.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 
 namespace GildedRose
 {
@@ -22,8 +24,77 @@ namespace GildedRose
         /// </summary>
         public void UpdateQuality()
         {
-            // TODO ...
-            // Hint: Iterate through this.items and check Name property to access specific item
+            foreach (var itm in items)
+            {
+                int a = 1;
+                if (itm.SellIn <= 0)
+                {
+                    a = 2;
+                }
+
+                if (itm.Name == "Sulfuras, Hand of Ragnaros")
+                {
+                    continue;
+                }
+
+                else if (itm.Name == "Aged Brie")
+                {
+                    itm.Quality += a;
+                }
+
+
+                else if (itm.Name == "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    if (itm.SellIn <= 0)
+                    {
+                        itm.Quality = 0;
+                    }
+
+                    else if (itm.SellIn > 10)
+                    {
+                        itm.Quality += a;
+                    }
+
+                    else if (itm.SellIn <= 5)
+                    {
+                        itm.Quality += a * 3;
+                    }
+
+                    else if (itm.SellIn <= 10)
+                    {
+                        itm.Quality += a * 2;
+                    }
+
+                }
+
+                else if (itm.Name == "Conjured Mana Cake")
+                {
+                    itm.Quality -= a * 2;
+                }
+
+                else if (itm.Name == "+5 Dexterity Vest" || itm.Name == "Elixir of the Mongoose")
+                {
+                    itm.Quality -= a;
+                }
+
+
+
+                itm.SellIn -= 1;
+
+
+                if (itm.Quality > 50)
+                {
+                    itm.Quality = 50;
+                }
+
+                if (itm.Quality < 0)
+                {
+                    itm.Quality = 0;
+                }
+
+
+
+            }
         }
     }
 }
